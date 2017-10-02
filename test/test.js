91f9e237812be1dc2d7c1613d59cd4648ecaed58
fatouche <foucaud.giovanny@gmail.com>
2017-10-02 11:45:01 +0200

    Histoire 2 -> Test Fini

--- test/test.js
+++ test/test.js
@@ -2,7 +2,23 @@
 
 var LyngkTestCase = TestCase("LyngkTestCase");
 
-LyngkTestCase.prototype.testA = function(){
-    var cel = new Lyngk.Coordinates('A',1);
+LyngkTestCase.prototype.testA = function () {
+    var cel = new Lyngk.Coordinates('A', 1);
     assertFalse(cel.valid());
+};
+
+LyngkTestCase.prototype.testB = function () {
+    var cmpt = 0;
+    var cel;
+
+    for (var col = 65; col <= 74; col++) {
+        for (var lig = 1; lig <= 9; lig++) {
+            cel = new Lyngk.Coordinates(String.fromCharCode(col), lig);
+            if (cel.valid()) {
+                cmpt = cmpt + 1;
+            }
+        }
+    }
+
+    assertEquals(cmpt, 43);
 };
