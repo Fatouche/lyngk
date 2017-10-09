01eaa78a6b0f0b6e97dc1002f22ef5facc0600fb
fatouche <foucaud.giovanny@gmail.com>
2017-10-09 11:36:58 +0200

    Histoire 8 -> code Fini

--- src/Intersection.js
+++ src/Intersection.js
@@ -4,11 +4,20 @@ Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
 Lyngk.Intersection = function () {
 
+    var piece;
     var celState = Lyngk.State.VACANT;
 
     this.getState = function(){
         return celState;
     };
-    
+
+    this.getColor = function(){
+        return piece.getColor();
+    };
+
+    this.pose = function(color) {
+        piece = new Lyngk.Piece(color);
+        celState = Lyngk.State.ONE_PIECE;
+    };
 
 };
