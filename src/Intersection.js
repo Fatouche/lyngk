6c585e83d658def6a7e2641400233f600470a440
fatouche <foucaud.giovanny@gmail.com>
2017-10-09 12:01:12 +0200

    Histoire 10 -> Fin Heure

--- src/Intersection.js
+++ src/Intersection.js
@@ -4,7 +4,8 @@ Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
 Lyngk.Intersection = function () {
 
-    var piece;
+    var piece = [];
+    var color;
     var celState = Lyngk.State.VACANT;
 
     this.getState = function(){
@@ -16,13 +17,18 @@ Lyngk.Intersection = function () {
     };
 
     this.pose = function(color) {
-        if(!piece) {
-            piece = new Lyngk.Piece(color);
+        if(piece.length <=0) {
             celState = Lyngk.State.ONE_PIECE;
-        }else{
-            piece = new Lyngk.Piece(color);
+        }
+        else if(piece.length > 0 && piece.length < 4) {
             celState = Lyngk.State.STACK;
         }
+        else if(piece.length >= 4) {
+            celState = Lyngk.State.FULL_STACK;
+        }
+
+        piece.push(new Lyngk.Piece(color));
+        color = color;
     };
 
 };
