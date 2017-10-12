2c460ee0a101729bd67676c85e8d136f829cf22d
factouche <foucaud.giovanny@gmail.com>
2017-10-12 14:46:54 +0200

    Histoire 11 -> code Fini

--- src/Engine.js
+++ src/Engine.js
@@ -4,4 +4,34 @@
 Lyngk.Color = {BLACK: 0, IVORY: 1, BLUE: 2, RED: 3, GREEN: 4, WHITE: 5};
 
 Lyngk.Engine = function () {
+
+    var coordonneeInterssection = [];
+
+    this.initPlateau = function()
+    {
+        for (var coordo in coordonneeInterssection) {
+            if (coordonneeInterssection.hasOwnProperty(coord))
+            {
+                coordonneeInterssection[coord].poser(Lyngk.Color.IVORY);
+            }
+        }
+    };
+
+    this.plateau1PieceFull = function()
+    {
+        for (var coord in coordonneeInterssection) {
+            if (coordonneeInterssection.hasOwnProperty(coord))
+            {
+                if(coordonneeInterssection[coord].getState() != Lyngk.State.ONE_PIECE)
+                    return false;
+            }
+        }
+        return true;
+    };
+
+    this.plateau = function()
+    {
+        return coordonneeInterssection;
+    };
+
 };
